5bd217dceb318810d60556a54cbb3b51a18682b9
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-13 18:39:00 +0300

    done with testing

--- __tests__/auth.router.test.js
+++ __tests__/auth.router.test.js
@@ -83,4 +83,158 @@ describe('Auth Model',()=>{
 
 });
 
+describe('testing extar routes',()=>{
+ 
+  it('GET to /read', ()=>{
+    let obj = {'username': 'omar', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        // console.log('4444444444444',result.body);
 
+        return mockRequest.get('/read').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            // console.log('55555555555555' ,data);
+  
+            expect(data.status).toEqual(200);
+            expect(data.text).toEqual('OK!!!');
+
+          });
+
+      });
+      
+  });
+
+  it('GET to /add', ()=>{
+    let obj = {'username': 'alia', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        // console.log('4444444444444',result.body);
+
+        return mockRequest.post('/add').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            // console.log('55555555555555' ,data);
+  
+            expect(data.status).toEqual(200);
+            expect(data.text).toEqual('OK!!!');
+
+          });
+
+      });
+      
+  });
+  it('PUT to /change', ()=>{
+    let obj = {'username': 'osama', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        // console.log('4444444444444',result.body);
+
+        return mockRequest.put('/change').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            // console.log('55555555555555' ,data);
+  
+            expect(data.status).toEqual(200);
+            expect(data.text).toEqual('OK!!!');
+
+          });
+
+      });
+      
+  });
+  it('delete to /remove', ()=>{
+    let obj = {'username': 'monther', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        // console.log('4444444444444',result.body);
+
+        return mockRequest.delete('/remove').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            // console.log('55555555555555' ,data.text,'yyy',data.body);
+  
+            expect(data.status).toEqual(200);
+            expect(data.text).toEqual('OK!!!');
+
+          });
+
+      });
+      
+  });
+
+  it('500 err + Access Denied!! to delete method for writer', ()=>{
+    let obj = {'username': 'ahmad', 'password': '1234','role':'writer'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        // console.log('4444444444444',result.body);
+
+        return mockRequest.delete('/remove').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            // console.log('55555555555555' ,data.text,'yyy',data.body);
+  
+            expect(data.status).toEqual(500);
+            expect(data.body).toEqual({ error: 'Access Denied!!'});
+
+          });
+
+      });
+      
+  });
+  it('hhhhhhhhhhh', ()=>{
+    let obj = {'username': 'muhannad', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        console.log('4444444444444',result.body);
+
+        return mockRequest.get('/secret').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            console.log('55555555555555' ,data.body);
+  
+            expect(data.status).toEqual(200);
+            // expect(data.text).toEqual('OK!!!');
+
+          });
+
+      });
+      
+  });
+
+  it('GET to /secret', ()=>{
+    let obj = {'username': 'ali', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        return mockRequest.get('/secret').set({ 'authorization':`Bearer ${result.body.token}`})
+          .then(data=>{
+            expect(data.status).toEqual(200);
+            expect(data.body.username).toEqual(obj.username);
+
+          });
+
+      });
+      
+  });
+ 
+  it('Invalid auth header ', ()=>{
+    let obj = {'username': 'muhannad', 'password': '1234','role':'admin'};
+    return  mockRequest.post('/signup')
+      .send(obj)
+      .then(result=>{
+        // console.log('4444444444444',result.body);
+
+        return mockRequest.get('/secret').set({ 'authorization':` ${result.body.token}`})
+          .then(data=>{
+            // console.log('8888888888' ,data.body);
+  
+            expect(data.status).toEqual(500);
+            expect(data.body).toEqual({error: 'Invalid auth header'});
+
+          });
+
+      });
+      
+  });
+});
